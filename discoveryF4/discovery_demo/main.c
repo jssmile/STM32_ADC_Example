0c626198274487636e742e4f1f40e1c7e37b67ff
jssmile <jssmile19921112@gmail.com>
2015-12-01 16:48:39 +0800

    Add the DMA

--- discoveryF4/discovery_demo/main.c
+++ discoveryF4/discovery_demo/main.c
@@ -11,10 +11,7 @@
 #include "stm32f4xx_adc.h"
 #include "stm32f4xx_usart.h"
 #include "usart.h"
-
-/** @addtogroup STM32F4-Discovery_Demo
-  * @{
-  */
+#include "stm32f4xx_dma.h"
 
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
@@ -25,6 +22,9 @@
 #define BUFFERSIZE 128
  
 uint16_t ADCConvertedValues[BUFFERSIZE];
+uint32_t ADC_ConvertedValue[2];
+uint32_t tmp1 = 5;
+uint32_t tmp2 = 10;;
 
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
@@ -47,14 +47,17 @@ uint8_t Buffer[6];
 int ConvertedValue = 0; //Converted value readed from ADC
 
 
-/* Private function prototypes -----------------------------------------------*/
-static uint32_t Demo_USBConfig(void);
+//static uint32_t Demo_USBConfig(void);
 //static void TIM4_Config(void);
 
 /* Private functions ---------------------------------------------------------*/
 int adc_convert();
 void adc_configure();
 void GPIO_PIN_INIT(void);
+void DMA_configure();
+
+//dma
+void DMAInit(void);
 
 inline int conv2temp(uint16_t value){
   int result = 0;
@@ -62,34 +65,23 @@ inline int conv2temp(uint16_t value){
 }
 
 //static uint16_t testing=10;
-
-/**
-  * @brief  Main program.
-  * @param  None
-  * @retval None
-  */
   void initialize()
   {
     init_USART3(9600);
-    USART_puts(USART3, "hello\n\r");
-
+    //USART_puts(USART3, "hello\n\r");
   }
 int main(void)
 {
   
   initialize();
   RCC_ClocksTypeDef RCC_Clocks;
-  /* Initialize LEDs and User_Button on STM32F4-Discovery --------------------*/
-  STM_EVAL_PBInit(BUTTON_USER, BUTTON_MODE_EXTI); 
-  
-  STM_EVAL_LEDInit(LED4);
   
   /* SysTick end of count event each 10ms */
   RCC_GetClocksFreq(&RCC_Clocks);
   SysTick_Config(RCC_Clocks.HCLK_Frequency / 100);
   
     /* Turn on LEDs available on STM32F4-Discovery ---------------------------*/
-  STM_EVAL_LEDOn(LED4);
+  //STM_EVAL_LEDOn(LED4);
 
   if (TimingDelay == 0x00)
   {
@@ -109,7 +101,13 @@ int main(void)
     while(1){
       GPIO_ResetBits(GPIOE, GPIO_Pin_3|GPIO_Pin_4|GPIO_Pin_5|GPIO_Pin_6|GPIO_Pin_7|GPIO_Pin_8|GPIO_Pin_9|GPIO_Pin_10|GPIO_Pin_11|GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14);
 
-      ConvertedValue = adc_convert();   //Read the ADC converted value
+      //ConvertedValue = adc_convert();   //Read the ADC converted value
+      //ADC_ConvertedValue = adc_convert();
+      ADC_SoftwareStartConv(ADC1);//Start the conversion
+      tmp1 = ADC_ConvertedValue[0];
+      tmp2 = ADC_ConvertedValue[1];
+      tmp1 = adc_convert();
+      //tmp2 = adc_convert();
       uint16_t sum = 0;
       
       register int i;
@@ -117,47 +115,87 @@ int main(void)
         sum|=(ConvertedValue & (1 << i)?USING_PIN[i]:0);
       
       GPIO_SetBits(GPIOE, sum);
-      USART_putd(USART3, ConvertedValue);
+      //USART_putd(USART3, ConvertedValue);
+      USART_puts(USART3, "PC0 : \n\r");
+      USART_putd(USART3, tmp1);
+      USART_puts(USART3, "\n\r");
+      USART_puts(USART3, "PC1 : \n\r");
+      USART_putd(USART3, tmp2);
       USART_puts(USART3, "\n\r");  
-      Delay(1);
+      Delay(100);
     }
 
 /* Try to test ADC.*/
   }
   else{}
 }
+
+void DMA_configure(){
+        DMA_InitTypeDef DMA_InitStructure;
+        DMA_DeInit(DMA1_Stream0);
+        DMA_InitStructure.DMA_Channel = DMA_Channel_0;  /*ADC1*/
+        DMA_InitStructure.DMA_PeripheralBaseAddr = (unsigned int)0x4001204C;
+        DMA_InitStructure.DMA_Memory0BaseAddr =(unsigned int) & ADC_ConvertedValue;
+        DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralToMemory;
+        DMA_InitStructure.DMA_BufferSize = 2;
+        DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Enable;
+        DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+        DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
+        DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
+        DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
+        DMA_InitStructure.DMA_Priority = DMA_Priority_High;
+        DMA_InitStructure.DMA_FIFOMode = DMA_FIFOMode_Disable; 
+        DMA_InitStructure.DMA_FIFOThreshold = DMA_FIFOThreshold_HalfFull;
+        DMA_InitStructure.DMA_MemoryBurst = DMA_MemoryBurst_Single;
+        //DMA_InitStructure.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;
+        DMA_Init(DMA2_Stream0, &DMA_InitStructure);
+        //DMA_Cmd(DMA2_Stream0, ENABLE);
+}
  
 void adc_configure(){
+  DMA_configure();
   ADC_InitTypeDef ADC_init_structure; //Structure for adc confguration
   GPIO_InitTypeDef GPIO_initStructre; //Structure for analog input pin
+  ADC_CommonInitTypeDef ADC_CommonInitStructure;
 
   //Clock configuration
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA2, ENABLE);
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);//ADC1 is connected to APB2 peripheral bus
-  RCC_AHB1PeriphClockCmd(RCC_AHB1ENR_GPIOCEN, ENABLE);//Clock for the ADC port!! Do not forget about this one ;)
-
+  
   //Analog input pin configuration
-  GPIO_initStructre.GPIO_Pin = GPIO_Pin_0;//The channel 10 is connected to PC0
+  RCC_AHB1PeriphClockCmd(RCC_AHB1ENR_GPIOCEN, ENABLE);//Clock for the ADC port!! Do not forget about this one ;)
+  GPIO_initStructre.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;//The channel 10 is connected to PC0
   GPIO_initStructre.GPIO_Mode = GPIO_Mode_AN; //The PC0 pin is configured in analog mode
   GPIO_initStructre.GPIO_PuPd = GPIO_PuPd_NOPULL; //We don't need any pull up or pull down
   GPIO_Init(GPIOC, &GPIO_initStructre);
 
+   /* ADC 常見的初始化 **********************************************************/
+    ADC_CommonInitStructure.ADC_Mode = ADC_Mode_Independent;
+    ADC_CommonInitStructure.ADC_Prescaler = ADC_Prescaler_Div2;
+    ADC_CommonInitStructure.ADC_DMAAccessMode = ADC_DMAAccessMode_Disabled;
+    ADC_CommonInitStructure.ADC_TwoSamplingDelay = ADC_TwoSamplingDelay_5Cycles;
+    ADC_CommonInit(&ADC_CommonInitStructure);
+
   //ADC structure configuration
   ADC_DeInit();//reset all parameters to their default values
   ADC_init_structure.ADC_DataAlign = ADC_DataAlign_Right;//converted data will be shifted to right
-  ADC_init_structure.ADC_Resolution = ADC_Resolution_12b;//Input voltage is converted into a 12-bit number whose maximum value is 4095
+  ADC_init_structure.ADC_Resolution = ADC_Resolution_10b;//Input voltage is converted into a 10-bit number whose maximum value is 1023
   ADC_init_structure.ADC_ContinuousConvMode = ENABLE; //the conversion is continuous, the input data is converted more than once
   ADC_init_structure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_T1_CC1;//use timer 1 capture/compare channel 1 for external trigger
   ADC_init_structure.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;//no trigger for conversion
-  ADC_init_structure.ADC_NbrOfConversion = 1;//Number of used ADC channels
-  ADC_init_structure.ADC_ScanConvMode = DISABLE;//No scan (only one channel)
+  ADC_init_structure.ADC_NbrOfConversion = 2;//Number of used ADC channels
+  ADC_init_structure.ADC_ScanConvMode = ENABLE;//No scan (only one channel)
   ADC_Init(ADC1, &ADC_init_structure);
 
   // use channel 10 from ADC1, with sample time 144 cycles
+  //ADC_RegularChannelConfig(ADC1, ADC_Channel_10, 1, ADC_SampleTime_144Cycles);
+  ADC_RegularChannelConfig(ADC1, ADC_Channel_11, 2, ADC_SampleTime_144Cycles);
   ADC_RegularChannelConfig(ADC1, ADC_Channel_10, 1, ADC_SampleTime_144Cycles);
-  //ADC_RegularChannelConfig(ADC1, ADC_Channel_TempSensor, 1, ADC_SampleTime_144Cycles);
   //ADC_TempSensorVrefintCmd(ENABLE);
 
   ADC_Cmd(ADC1, ENABLE);
+  ADC_DMARequestAfterLastTransferCmd(ADC1, ENABLE);
+  ADC_DMACmd(ADC1, ENABLE);
 }
 
 int adc_convert(){
@@ -179,27 +217,6 @@ void GPIO_PIN_INIT(void){
     GPIO_Init( GPIOE, &GPIO_InitStructure ); 
 }
 
-/**
-  * @brief  Initializes the USB for the demonstration application.
-  * @param  None
-  * @retval None
-  */
-static uint32_t Demo_USBConfig(void)
-{
-  USBD_Init(&USB_OTG_dev,
-            USB_OTG_FS_CORE_ID,
-            &USR_desc, 
-            &USBD_HID_cb, 
-            &USR_cb);
-  
-  return 0;
-}
-
-/**
-  * @brief  Inserts a delay time.
-  * @param  nTime: specifies the delay time length, in 10 ms.
-  * @retval None
-  */
 void Delay(__IO uint32_t nTime)
 {
   TimingDelay = nTime;
@@ -207,11 +224,6 @@ void Delay(__IO uint32_t nTime)
   while(TimingDelay != 0);
 }
 
-/**
-  * @brief  Decrements the TimingDelay variable.
-  * @param  None
-  * @retval None
-  */
 void TimingDelay_Decrement(void)
 {
   if (TimingDelay != 0x00)
@@ -220,11 +232,6 @@ void TimingDelay_Decrement(void)
   }
 }
 
-/**
-  * @brief  This function handles the test program fail.
-  * @param  None
-  * @retval None
-  */
 void Fail_Handler(void)
 {
   /* Erase last sector */ 
@@ -240,11 +247,6 @@ void Fail_Handler(void)
   }
 }
 
-/**
-  * @brief  MEMS accelerometre management of the timeout situation.
-  * @param  None.
-  * @retval None.
-  */
 uint32_t LIS302DL_TIMEOUT_UserCallback(void)
 {
   /* MEMS Accelerometer Timeout error occured during Test program execution */
@@ -283,10 +285,5 @@ void assert_failed(uint8_t* file, uint32_t line)
   }
 }
 #endif
-
-/**
-  * @}
-  */
-
 
 /******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
