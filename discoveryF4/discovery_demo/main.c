4b1451409a1823044e70739cee0dd6f47f0edece
jssmile <jssmile19921112@gmail.com>
2015-12-04 00:08:55 +0800

    Measure the internal temperature channel value

--- discoveryF4/discovery_demo/main.c
+++ discoveryF4/discovery_demo/main.c
@@ -21,10 +21,10 @@
 #define ALLTEST_FAIL               0x55555555
 #define BUFFERSIZE 128
  
-uint16_t ADCConvertedValues[BUFFERSIZE];
+/*uint16_t ADCConvertedValues[BUFFERSIZE];
 uint32_t ADC_ConvertedValue[2];
 uint32_t tmp1 = 5;
-uint32_t tmp2 = 10;;
+uint32_t tmp2 = 10;;*/
 
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
@@ -61,7 +61,7 @@ void DMAInit(void);
 
 inline int conv2temp(uint16_t value){
   int result = 0;
-  return result = ( ( ( ( value * 2960 ) / 4096 ) - 760 ) / ( 25 / 10 ) ) + 25;
+  return result = ( ( ( ( value * 2910 ) / 4096 ) - 760 ) / ( 25 / 10 ) ) + 25;
 }
 
 //static uint16_t testing=10;
@@ -101,27 +101,27 @@ int main(void)
     while(1){
       GPIO_ResetBits(GPIOE, GPIO_Pin_3|GPIO_Pin_4|GPIO_Pin_5|GPIO_Pin_6|GPIO_Pin_7|GPIO_Pin_8|GPIO_Pin_9|GPIO_Pin_10|GPIO_Pin_11|GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14);
 
-      //ConvertedValue = adc_convert();   //Read the ADC converted value
+      ConvertedValue = adc_convert();   //Read the ADC converted value
       //ADC_ConvertedValue = adc_convert();
-      ADC_SoftwareStartConv(ADC1);//Start the conversion
-      tmp1 = ADC_ConvertedValue[0];
+      /*tmp1 = ADC_ConvertedValue[0];
       tmp2 = ADC_ConvertedValue[1];
-      tmp1 = adc_convert();
-      //tmp2 = adc_convert();
+      tmp1 = ADC_GetConversionValue(ADC1);
+      tmp2 = ADC_GetConversionValue(ADC1);*/
       uint16_t sum = 0;
       
       register int i;
       for(i=0; i<12; ++i)
         sum|=(ConvertedValue & (1 << i)?USING_PIN[i]:0);
-      
+      ConvertedValue = conv2temp(ConvertedValue);
       GPIO_SetBits(GPIOE, sum);
       //USART_putd(USART3, ConvertedValue);
-      USART_puts(USART3, "PC0 : \n\r");
-      USART_putd(USART3, tmp1);
+      USART_puts(USART3, "Celsius : \n\r");
+      USART_putd(USART3, ConvertedValue);
+      USART_puts(USART3, "℃");
       USART_puts(USART3, "\n\r");
-      USART_puts(USART3, "PC1 : \n\r");
+      /*USART_puts(USART3, "PC1 : \n\r");
       USART_putd(USART3, tmp2);
-      USART_puts(USART3, "\n\r");  
+      USART_puts(USART3, "\n\r"); */ 
       Delay(100);
     }
 
@@ -135,11 +135,11 @@ void DMA_configure(){
         DMA_DeInit(DMA1_Stream0);
         DMA_InitStructure.DMA_Channel = DMA_Channel_0;  /*ADC1*/
         DMA_InitStructure.DMA_PeripheralBaseAddr = (unsigned int)0x4001204C;
-        DMA_InitStructure.DMA_Memory0BaseAddr =(unsigned int) & ADC_ConvertedValue;
+        DMA_InitStructure.DMA_Memory0BaseAddr =(unsigned int) & ConvertedValue;
         DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralToMemory;
-        DMA_InitStructure.DMA_BufferSize = 2;
+        DMA_InitStructure.DMA_BufferSize = 1;
         DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Enable;
-        DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+        DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Disable;
         DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
         DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
         DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
@@ -153,18 +153,18 @@ void DMA_configure(){
 }
  
 void adc_configure(){
-  DMA_configure();
+  //DMA_configure();
   ADC_InitTypeDef ADC_init_structure; //Structure for adc confguration
   GPIO_InitTypeDef GPIO_initStructre; //Structure for analog input pin
   ADC_CommonInitTypeDef ADC_CommonInitStructure;
 
   //Clock configuration
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA2, ENABLE);
+  //RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA2, ENABLE);
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);//ADC1 is connected to APB2 peripheral bus
   
   //Analog input pin configuration
   RCC_AHB1PeriphClockCmd(RCC_AHB1ENR_GPIOCEN, ENABLE);//Clock for the ADC port!! Do not forget about this one ;)
-  GPIO_initStructre.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;//The channel 10 is connected to PC0
+  GPIO_initStructre.GPIO_Pin = GPIO_Pin_0;//The channel 10 is connected to PC0
   GPIO_initStructre.GPIO_Mode = GPIO_Mode_AN; //The PC0 pin is configured in analog mode
   GPIO_initStructre.GPIO_PuPd = GPIO_PuPd_NOPULL; //We don't need any pull up or pull down
   GPIO_Init(GPIOC, &GPIO_initStructre);
@@ -179,23 +179,25 @@ void adc_configure(){
   //ADC structure configuration
   ADC_DeInit();//reset all parameters to their default values
   ADC_init_structure.ADC_DataAlign = ADC_DataAlign_Right;//converted data will be shifted to right
-  ADC_init_structure.ADC_Resolution = ADC_Resolution_10b;//Input voltage is converted into a 10-bit number whose maximum value is 1023
+  ADC_init_structure.ADC_Resolution = ADC_Resolution_12b;//Input voltage is converted into a 10-bit number whose maximum value is 1023
   ADC_init_structure.ADC_ContinuousConvMode = ENABLE; //the conversion is continuous, the input data is converted more than once
   ADC_init_structure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_T1_CC1;//use timer 1 capture/compare channel 1 for external trigger
   ADC_init_structure.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;//no trigger for conversion
-  ADC_init_structure.ADC_NbrOfConversion = 2;//Number of used ADC channels
-  ADC_init_structure.ADC_ScanConvMode = ENABLE;//No scan (only one channel)
+  ADC_init_structure.ADC_NbrOfConversion = 1;//Number of used ADC channels
+  ADC_init_structure.ADC_ScanConvMode = DISABLE;//No scan (only one channel)
   ADC_Init(ADC1, &ADC_init_structure);
 
   // use channel 10 from ADC1, with sample time 144 cycles
   //ADC_RegularChannelConfig(ADC1, ADC_Channel_10, 1, ADC_SampleTime_144Cycles);
-  ADC_RegularChannelConfig(ADC1, ADC_Channel_11, 2, ADC_SampleTime_144Cycles);
-  ADC_RegularChannelConfig(ADC1, ADC_Channel_10, 1, ADC_SampleTime_144Cycles);
-  //ADC_TempSensorVrefintCmd(ENABLE);
+  ADC_RegularChannelConfig(ADC1, ADC_Channel_TempSensor, 1, ADC_SampleTime_144Cycles);
+  //ADC_RegularChannelConfig(ADC1, ADC_Channel_11, 2, ADC_SampleTime_144Cycles);
+
+  ADC_TempSensorVrefintCmd(ENABLE);
 
   ADC_Cmd(ADC1, ENABLE);
-  ADC_DMARequestAfterLastTransferCmd(ADC1, ENABLE);
-  ADC_DMACmd(ADC1, ENABLE);
+  //ADC_DMARequestAfterLastTransferCmd(ADC1, ENABLE);
+  //ADC_DMACmd(ADC1, ENABLE);
+  //ADC_SoftwareStartConv(ADC1);
 }
 
 int adc_convert(){
